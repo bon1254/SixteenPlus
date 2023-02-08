a09443374d87d12ed68f97e67c948e5a08ed0f6d
bon1254 <bon1254@gmail.com>
2023-02-08 15:34:45 +0800

    分批上傳6

---
+++ Assets/Scripts/LoginManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LCGoogleLogin;
+using Sirenix.OdinInspector;
+
+public class LoginManager : MonoBehaviour
+{
+    [LabelText("µn¤J«ö¶s")] public GameObject Btn_Login;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Login()
+    {
+
+    }
+}
